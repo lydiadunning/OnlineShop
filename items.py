c89bcad579ec63d48bbf5afdbe4bf72110a6c222
Lydia Dunning <lydia.dunning@gmail.com>
2022-06-16 14:01:40 -0700

    Rough working first draft Integrate ORM Add shopping cart functionality

---
+++ items.py
@@ -0,0 +1,38 @@
+import string
+from urllib.parse import quote_plus
+from main import Item, db
+from random import choices, randrange
+
+# Use this as a tool to populate a placeholder database of items for sale
+
+def make_placeholder_data(quantity):
+    chars = f"{string.digits} {string.ascii_letters}"
+
+    for i in range(quantity):
+        name = "".join(choices(chars, k=randrange(3, 12)))
+        price = "".join(choices(string.digits, k=randrange(1, 3))) + "." + "".join(choices(string.digits, k=2))
+        description = "".join(choices(chars, k=randrange(100, 200)))
+        new_item = Item(name=name,
+                        url=quote_plus(name),
+                        image="static/placeholder.png",
+                        price=price,
+                        description=description,
+                        available=randrange(0, 5)
+                        )
+        db.session.add(new_item)
+        db.session.commit()
+        print(f"name: {name}")
+        print(f"url: {quote_plus(name)}")
+        print(f"image: static/placeholder.png")
+        print(f"price: ${price}")
+        print(f"description: {description}")
+        print(f"available: {randrange(0, 5)}")
+
+# make_placeholder_data(10)
+
+def fill_inventory(amount = 10):
+    for item in Item.query.all():
+        item.available += amount
+        db.session.commit()
+
+fill_inventory()
--- main.py
+++ main.py
@@ -1,48 +1,91 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # I don't fully understand why this web app requires a login.
 
-class Item:
-    # gets item info from ORM
-    pass
-
-
 app = Flask(__name__)
 app.secret_key = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    # A single item for sale
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    url = db.Column(db.String(20), unique=True, nullable=False)
+    image = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.String(10), nullable=False)
+    description = db.Column(db.String(200))
+    available = db.Column(db.Integer)
+
+db.create_all()
+
 
 # cart_dict is a placeholder for a session based shopping cart
 cart_dict = {}
 
+
 @app.route('/')
 def home():
-    return render_template('item_list.html', items=Item.query.filter_by(available=True).all())
+    return render_template('item_list.html', items=Item.query.all())
 
-@app.route('/<item_name>', methods=['GET', 'POST'])
-def item(item_name):
-    item = Item.query.filter_by(item_name=item_name).first()
-    return render_template('item.html', item=item, available=item and item.available)
 
-@app.route('/add/<link_back>/<item_id>', methods=['GET', 'POST'])
-def add(link_back, item_id):
+@app.route('/<name_url>', methods=['GET', 'POST'])
+def item(name_url):
+    item_to_show = Item.query.filter_by(url=name_url).first()
+    if item_to_show.id in cart_dict.keys():
+        amount_in_cart = cart_dict[item_to_show.id]
+    else:
+        amount_in_cart = 0
+    return render_template('item.html',
+                           item=item_to_show,
+                           available=item_to_show and item_to_show.available,
+                           amount_in_cart=amount_in_cart)
+
+
+@app.route('/add/<item_id>', methods=['GET', 'POST'])
+def add(item_id):
     new_item = Item.query.get(item_id)
-    if cart_dict[item_id]:
-        cart_dict[item_id] += 1
+    if new_item.available:
+        if int(item_id) in cart_dict.keys():
+            cart_dict[int(item_id)] += 1
+            new_item.available -= 1
+            db.session.commit()
+        else:
+            cart_dict[int(item_id)] = 1
+            new_item.available -= 1
+            db.session.commit()
     else:
-        cart_dict[item_id] = 1
-    return redirect(link_back, item=new_item, available=item and item.available)
+        # replace this print statement
+        print("Out of stock")
+    return redirect(url_for("item", name_url=new_item.url))
 
-@app.route('/remove/<link_back>/<item_id>', methods=['GET', 'POST'])
-def remove(link_back, item_id):
-    item_to_remove = Item.query.get(item_id)
-    if cart_dict[item_id] > 1:
-        cart_dict[item_id] -= 1
-    else:
-        del cart_dict[item_id]
-    return redirect(link_back, item=item_to_remove, available=item and item.available)
+
+@app.route('/remove/<item_id>', methods=['GET', 'POST'])
+def remove(item_id):
+    id = int(item_id)
+    item_to_remove = Item.query.get(id)
+    if id in cart_dict.keys():
+        if cart_dict[id] > 1:
+            cart_dict[id] -= 1
+            item_to_remove.available += 1
+            db.session.commit()
+        else:
+            del cart_dict[id]
+            item_to_remove.available += 1
+            db.session.commit()
+    return redirect(url_for("item", name_url=item_to_remove.url))
+
 
 @app.route('/cart')
 def cart():
-    return render_template('cart.html', in_cart=cart_dict)
+    item_list = []
+    for item_id in cart_dict.keys():
+        item_list.append(Item.query.get(item_id))
+    return render_template('cart.html', cart_dict=cart_dict, items=item_list)
+
 
 @app.route('/checkout', methods=['GET', 'POST'])
 def checkout():
@@ -53,9 +96,11 @@ def checkout():
         return redirect()
     return render_template('checkout.html')
 
+
 @app.route('/checkout_complete')
 def checkout_complete():
     return render_template('checkout_complete.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
