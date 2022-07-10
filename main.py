5390fdf9061515e9f33dec077401407da91f50ef
Lydia Dunning <lydia.dunning@gmail.com>
2022-07-09 20:49:04 -0700

    Add 404 messaging accounting for variables in route Try new styling, including nav bar Add total price in shopping cart and nav bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import string
 
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, redirect, request, url_for, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote_plus, unquote_plus
 import os
@@ -42,32 +42,45 @@ db.create_all()
 
 def remove_from_cart(item_id, canceled_or_purchased, quantity=1):
     item_to_remove = Item.query.get(item_id)
-    print('remove_from_cart: ' + str(session['cart_dict']))
-    if item_id in session['cart_dict'].keys():
-        if session['cart_dict'][item_id] > quantity:
-            session['cart_dict'][item_id] -= quantity
+    if item_to_remove:
+        if item_id in session['cart_dict'].keys():
+            if session['cart_dict'][item_id] > quantity:
+                session['cart_dict'][item_id] -= quantity
+                if canceled_or_purchased == 'canceled':
+                    item_to_remove.available += quantity
+                    item_to_remove.in_a_cart -= quantity
+                elif canceled_or_purchased == 'purchased':
+                    item_to_remove.in_a_cart -= quantity
+            elif session['cart_dict'][item_id] == quantity:
+                del session['cart_dict'][item_id]
+                if canceled_or_purchased == 'canceled':
+                    item_to_remove.available += quantity
+                    item_to_remove.in_a_cart -= quantity
+                elif canceled_or_purchased == 'purchased':
+                    item_to_remove.in_a_cart -= quantity
+            db.session.commit()
             session.modified = True
-            if canceled_or_purchased == 'canceled':
-                item_to_remove.available += quantity
-                item_to_remove.in_a_cart -= quantity
-            elif canceled_or_purchased == 'purchased':
-                item_to_remove.in_a_cart -= quantity
-            db.session.commit()
-        elif session['cart_dict'][item_id] == quantity:
-            del session['cart_dict'][item_id]
-            print("cart_dict if cart value == quantity " + str(session['cart_dict']))
-            if canceled_or_purchased == 'canceled':
-                item_to_remove.available += quantity
-                item_to_remove.in_a_cart -= quantity
-            elif canceled_or_purchased == 'purchased':
-                item_to_remove.in_a_cart -= quantity
-            db.session.commit()
-    # returns the name of the item to avoid unnecessary database calls.
-    return session['cart_dict']
+        # returns the name of the item to avoid unnecessary database calls.
+        return quote_plus(item_to_remove.name)
+    else:
+        return
+
 
 def clear_cart(cancelled_or_purchased):
     for item_id, item_quantity in session['cart_dict'].items():
          remove_from_cart(item_id, cancelled_or_purchased, item_quantity)
+
+def return_cart_total():
+    if session['cart_dict']:
+        total = 0
+        for item_id, item_quantity in session['cart_dict'].items():
+            cart_item = Item.query.get(item_id)
+            price = float(cart_item.price)
+            total += (price * item_quantity)
+        return '{:#.2f}'.format(total)
+    else:
+        return '0.00'
+
 
 
 @app.route('/')
@@ -78,57 +91,63 @@ def home():
     for item in items:
         item.url = quote_plus(item.name)
     print('home: ' + str(session['cart_dict']))
-    return render_template('item_list.html', items=items, cart_dict=session['cart_dict'])
+    return render_template('item_list.html', items=items, cart_dict=session['cart_dict'], cart_total=return_cart_total())
 
 
-@app.route('/item/<name_url>', methods=['GET', 'POST'])
+@app.route('/<name_url>', methods=['GET', 'POST'])
 def item(name_url):
     name = unquote_plus(name_url)
     item_to_show = Item.query.filter_by(name=name).first()
-    if 'cart_dict' not in session:
-        session['cart_dict'] = {}
-        session.modified = True
-    if str(item_to_show.id) in session['cart_dict']:
-        amount_in_cart = session['cart_dict'][str(item_to_show.id)]
+    if item_to_show:
+        if 'cart_dict' not in session:
+            session['cart_dict'] = {}
+            session.modified = True
+        if str(item_to_show.id) in session['cart_dict']:
+            amount_in_cart = session['cart_dict'][str(item_to_show.id)]
+        else:
+            amount_in_cart = 0
+        return render_template('item.html',
+                               item=item_to_show,
+                               available=item_to_show and item_to_show.available,
+                               amount_in_cart=amount_in_cart,
+                               cart_total=return_cart_total())
     else:
-        amount_in_cart = 0
-    return render_template('item.html',
-                           item=item_to_show,
-                           available=item_to_show and item_to_show.available,
-                           amount_in_cart=amount_in_cart)
+        abort(404)
 
 
 @app.route('/add/<source>/<item_id>', methods=['GET', 'POST'])
 def add(source, item_id):
     new_item = Item.query.get(item_id)
-    new_item_url = quote_plus(new_item.name)
-    if new_item.available:
-        if item_id in session['cart_dict']:
-            session['cart_dict'][item_id] += + 1
-            session.modified = True
-            new_item.available -= 1
-            new_item.in_a_cart += 1
-            db.session.commit()
+    if source and new_item:
+        new_item_url = quote_plus(new_item.name)
+        if new_item.available:
+            if item_id in session['cart_dict']:
+                session['cart_dict'][item_id] += + 1
+                session.modified = True
+                new_item.available -= 1
+                new_item.in_a_cart += 1
+                db.session.commit()
+            else:
+                session['cart_dict'][item_id] = 1
+                session.modified = True
+                new_item.available -= 1
+                new_item.in_a_cart += 1
+                db.session.commit()
         else:
-            session['cart_dict'][item_id] = 1
-            session.modified = True
-            new_item.available -= 1
-            new_item.in_a_cart += 1
-            db.session.commit()
+            # replace this print statement
+            print("Out of stock")
+        return redirect(url_for(source, name_url=new_item_url))
     else:
-        # replace this print statement
-        print("Out of stock")
-    return redirect(url_for(source, name_url=new_item_url))
-
+        abort(404)
 
 @app.route('/remove/<source>/<item_id>', methods=['GET', 'POST'])
 def remove(source, item_id):
-    item_to_remove_url = quote_plus(Item.query.get(item_id).name)
-    session['cart_dict'] = remove_from_cart(item_id, 'canceled', 1)
-    print('source: ' + source )
-    print('session before redirect: ' + str(session['cart_dict']))
-    return redirect(url_for(source, name_url=item_to_remove_url))
-
+    item_to_remove_url = remove_from_cart(item_id, 'canceled', 1)
+    if source and item_to_remove_url:
+        print('source: ' + source )
+        print('session before redirect: ' + str(session['cart_dict']))
+        return redirect(url_for(source, name_url=item_to_remove_url))
+    abort(404)
 
 @app.route('/cart')
 def cart():
@@ -143,7 +162,7 @@ def cart():
         print(item.id)
         print(type(item.id))
 
-    return render_template('cart.html', cart_dict=session['cart_dict'], items=item_list)
+    return render_template('cart.html', cart_dict=session['cart_dict'], items=item_list, cart_total=return_cart_total())
 
 def price_fix(price: string) -> int:
     return int(price.replace('.', ''))
@@ -169,9 +188,9 @@ def create_checkout_session():
 
     return redirect(checkout_session.url, code=303)
 
-@app.route('/checkout/success')
+@app.route('/success')
 def success():
-    return render_template('success.html')
+    return render_template('success.html', cart_total=return_cart_total())
 
 @app.route('/checkout/cancel')
 def cancel():
